9102eb92f01d37acd2bdedfcf131f56d797408e8
Don Kackman <dkackman@gmail.com>
2024-02-11 08:58:08 -0600

    initial

---
+++ src/ServerCoin/ServerCoinFactory.cs
@@ -0,0 +1,13 @@
+﻿using chia.dotnet;
+
+namespace ServerCoin;
+
+public class ServerCoinFactory(EndpointInfo fullNodeHost)
+{
+    private readonly EndpointInfo _fullNodeHost = fullNodeHost;
+
+    public CoinSpend CreateCoinSpend()
+    {
+        return new CoinSpend();
+    }
+}
