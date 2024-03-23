b42cca57b9e9482e2644651057084ca87af36ac0
Don Kackman <dkackman@gmail.com>
2024-03-23 14:43:40 -0500

    delete server coin

--- src/ServerCoin/ServerCoinFactory.cs
+++ src/ServerCoin/ServerCoinFactory.cs
@@ -8,15 +8,18 @@ namespace dig.servercoin;
 
 public class ServerCoinFactory
 {
+    private readonly FullNodeProxy _fullNode;
+    private readonly StandardWallet _wallet;
     private readonly Program mirrorPuzzle = Puzzles.LoadPuzzle("p2_parent");
     private readonly Program curriedMirrorPuzzle;
     private readonly Program curriedMirrorPuzzleFromHex;
 
-    public ServerCoinFactory()
+    public ServerCoinFactory(FullNodeProxy fullNode, StandardWallet wallet)
     {
+        _fullNode = fullNode;
+        _wallet = wallet;
         curriedMirrorPuzzle = mirrorPuzzle.Curry([Program.FromInt(1)]);
-        var curriedMirrorPuzzleHex = HexHelper.SanitizeHex(curriedMirrorPuzzle.HashHex());
-        curriedMirrorPuzzleFromHex = Program.FromHex(curriedMirrorPuzzleHex);
+        curriedMirrorPuzzleFromHex = Program.FromHex(HexHelper.SanitizeHex(curriedMirrorPuzzle.HashHex()));
     }
 
     public Coin CreateServerCoin(Program launcherId, IEnumerable<CoinRecord> coinRecords, ulong amount, ulong fee)
@@ -35,18 +38,98 @@ public class ServerCoinFactory
         return null;
     }
 
-    public async Task<IEnumerable<ServerCoin>> GetServerCoins(FullNodeProxy fullNode, StandardWallet wallet, string launcherId, CancellationToken token = default)
+    public async Task<bool> DeleteServerCoin(string coinId, string genesisChallenge, ulong fee, CancellationToken token = default)
+    {
+        var coinRecordResponse = await _fullNode.GetCoinRecordByName(coinId, token);
+
+        var puzzleSolution = await _fullNode.GetPuzzleAndSolution(
+            coinRecordResponse.Coin.ParentCoinInfo,
+            coinRecordResponse.ConfirmedBlockIndex
+        );
+
+        var revealProgram = Program.DeserializeHex(HexHelper.SanitizeHex(puzzleSolution.PuzzleReveal));
+        var delegatedPuzzle = chia.dotnet.wallet.Puzzles.PayToConditions.Run(Program.FromList([Program.Nil])).Value;
+
+        var standardTransactionInnerSolution = Program.FromList([
+            Program.Nil,
+            delegatedPuzzle,
+            Program.Nil,
+        ]);
+
+        var coinRecords = _wallet.SelectCoinRecords(1 + fee, CoinSelection.Smallest);
+
+        if (coinRecords.Count == 0)
+        {
+            throw new Exception("Insufficient balance");
+        }
+
+        var totalValue = BigInteger.Zero;
+        foreach (var record in coinRecords)
+        {
+            totalValue += record.Coin.Amount;
+        }
+
+        var changeAmount = totalValue - fee;
+
+        var coinSpends = coinRecords.Select((coinRecord, index) =>
+        {
+            var spentPuzzle = _wallet.PuzzleCache.First(puzzle => puzzle.HashHex() == HexHelper.SanitizeHex(coinRecord.Coin.PuzzleHash));
+            var solution = new List<Program>();
+
+            if (index == 0)
+            {
+                // Send the change to the same address
+                solution.Add(
+                    Program.FromSource(
+                        $"({(int)ConditionCodes.CREATE_COIN} {HexHelper.FormatHex(coinRecord.Coin.PuzzleHash)} {changeAmount})"
+                    )
+                );
+            }
+
+            var coinSpend = new CoinSpend
+            {
+                Coin = coinRecord.Coin,
+                PuzzleReveal = spentPuzzle.SerializeHex(),
+                Solution = StandardTransaction.GetSolution(solution).SerializeHex(),
+            };
+
+            return coinSpend;
+        }).ToList();
+
+        var deleteCoinSpend = new CoinSpend
+        {
+            Coin = coinRecordResponse.Coin,
+            PuzzleReveal = curriedMirrorPuzzle.SerializeHex(),
+            Solution = Program.FromSource(
+                $"({puzzleSolution.Coin.ParentCoinInfo} {revealProgram} {puzzleSolution.Coin.Amount} {standardTransactionInnerSolution})"
+            ).SerializeHex(),
+        };
+
+        coinSpends.Add(deleteCoinSpend);
+
+        var spendBundle = new SpendBundle
+        {
+            CoinSpends = coinSpends,
+            AggregatedSignature = JacobianPoint.InfinityG2().ToHex(),
+        };
+
+        var signedSpendBundle = _wallet.SignSpend(spendBundle, genesisChallenge.FromHex());
+
+        return await _fullNode.PushTx(signedSpendBundle);
+    }
+
+    public async Task<IEnumerable<ServerCoin>> GetServerCoins(string launcherId, CancellationToken token = default)
     {
         var launcher = Program.FromHex(launcherId);
         var hint = Program.FromBigInt(launcher.ToBigInt() + 1)
             .ToHex()
             .PadLeft(64, '0')[..64];
-        var coinRecords = await fullNode.GetCoinRecordsByHint(hint, true, cancellationToken: token);
+        var coinRecords = await _fullNode.GetCoinRecordsByHint(hint, false, cancellationToken: token);
         var servers = new List<ServerCoin>();
 
         foreach (var coinRecord in coinRecords)
         {
-            var puzzleSolution = await fullNode.GetPuzzleAndSolution(
+            var puzzleSolution = await _fullNode.GetPuzzleAndSolution(
                 coinRecord.Coin.ParentCoinInfo,
                 coinRecord.ConfirmedBlockIndex,
                 token
@@ -58,12 +141,12 @@ public class ServerCoinFactory
             var createCoinConditions = conditions.ToList().Where(condition =>
                 condition.ToList().Count == 4 &&
                 condition.Rest.First.Equals(curriedMirrorPuzzleFromHex) &&
-                condition.First.ToInt() == 51
+                condition.First.ToInt() == (int)ConditionCodes.CREATE_COIN
             ).ToList();
 
             var urlString = createCoinConditions.Select(condition => condition.Rest.Rest.Rest.First.Rest);
             var urls = urlString.First().ToList().Select(url => url.ToText());
-            var ourPuzzle = wallet.PuzzleCache.FirstOrDefault(puzzle => puzzle.Equals(revealProgram));
+            var ourPuzzle = _wallet.PuzzleCache.FirstOrDefault(puzzle => puzzle.Equals(revealProgram));
 
             servers.Add(new ServerCoin
             {
@@ -74,6 +157,7 @@ public class ServerCoinFactory
                 Urls = urls,
             });
         }
+
         return servers;
     }
 }
--- src/Tests/ApiTests.cs
+++ src/Tests/ApiTests.cs
@@ -6,12 +6,11 @@ namespace Tests;
 
 public class ApiTests
 {
-    private const ulong RESERVE_AMOUNT = 300000;
+    private const ulong RESERVE_AMOUNT = 300000001;
 
     [SkippableFact]
     public async Task ListCoins()
     {
-        var x = Environment.GetEnvironmentVariable("CHIA_ROOT");
         Skip.If(Environment.GetEnvironmentVariable("CHIA_ROOT") is null);
 
         using CancellationTokenSource cts = new(100000);
@@ -27,9 +26,39 @@ public class ApiTests
         var wallet = new StandardWallet(fullNode, keyStore);
         await wallet.Sync(null, cts.Token);
 
-        var serverCoinFactory = new ServerCoinFactory();
-        var coins = await serverCoinFactory.GetServerCoins(fullNode, wallet, "26632ad912845e6f77714ca0996b21eb647803162dd444275ab5c09bea9426ea");
+        var serverCoinFactory = new ServerCoinFactory(fullNode, wallet);
+        var coins = await serverCoinFactory.GetServerCoins("26632ad912845e6f77714ca0996b21eb647803162dd444275ab5c09bea9426ea");
         Assert.NotEmpty(coins);
+    }
+
+    [SkippableFact]
+    public async Task DeleteCoin()
+    {
+        Skip.If(Environment.GetEnvironmentVariable("CHIA_ROOT") is null);
+
+        using CancellationTokenSource cts = new(100000);
+
+        var config = Config.Open();
+        var fullNodeEndpoint = config.GetEndpoint("full_node");
+        var fullNode = new FullNodeProxy(new HttpRpcClient(fullNodeEndpoint), "wallet.tests");
+
+        var walletEndpoint = config.GetEndpoint("wallet");
+        var walletProxy = new WalletProxy(new HttpRpcClient(walletEndpoint), "wallet.tests");
+        var keyStore = await KeyStore.CreateFrom(walletProxy);
+
+        var fee = await GetFee(fullNode);
+
+        var wallet = new StandardWallet(fullNode, keyStore);
+        await wallet.Sync(null, cts.Token);
+
+        var serverCoinFactory = new ServerCoinFactory(fullNode, wallet);
+        var result = await serverCoinFactory.DeleteServerCoin(
+            "4551a6bf78f523d1547d6948eda06aca1d275cb1b904c553a7bf32b437fdfcb9",
+            config.GetGenesisChallenge() ?? throw new Exception("Genesis challenge not found"),
+            fee,
+            cts.Token);
+
+        Assert.True(result);
     }
 
     private static async Task<ulong> GetFee(FullNodeProxy node)
